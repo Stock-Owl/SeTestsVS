ac1fe73eb21b57ca8db5edafda49d2b471ab6e77
Kós Márton <kosmarci73@gmail.com>
2023-06-30 11:20:08 +0200

    Initial commit

---
+++ SeTestsVS/SeTestsVS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeTestVSCore
+{
+    class Core
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+        }
+    }
+}
