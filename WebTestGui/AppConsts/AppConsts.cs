fe2d296f3df61209330ff99df9c5eeed7e964b2b
Ivánczi Balázs <balazsivanczi06@gmail.com>
2023-07-10 11:11:19 +0200

    Removed da GUI

--- WebTestGui/AppConsts/AppConsts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebTestGui.AppConsts
-{
-    internal static class AppConsts
-    {
-        public static readonly string s_AppName = "VS WebTeszt";
-        public static readonly string s_AppVersion = "v1.0";
-    }
-}
