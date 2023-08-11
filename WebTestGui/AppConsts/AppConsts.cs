cdc33256fba1ec426dfebbd79143de7c148ee0ae
Ivánczi Balázs <balazsivanczi06@gmail.com>
2023-08-11 13:53:54 +0200

    Everything up once again

---
+++ WebTestGui/AppConsts/AppConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace WebTestGui.AppConsts
+{
+    internal static class AppConsts
+    {
+        public static readonly string s_AppName = "VS WebTeszt";
+        public static readonly string s_AppVersion = "v1.0";
+    }
+}
---
+++ WebTestGui/Utils/TypeHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace WebTestGui.Utils
+{
+    public static class TypeHelpers
+    {
+        public static T EnumTypeFromString<T>(string text)
+        {
+            return (T)Enum.Parse(typeof(T), text);
+        }
+
+        public static object[] GetEnumTypes<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<object>().ToArray();
+        }
+
+        public static Type GetClassFromString(string classname)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.Name == classname)
+                    {
+                        return type;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static object[] GetAllSubClasses<T>()
+        {
+            Type[] allTypes = Assembly.GetAssembly(typeof(T))!.GetTypes();
+            List<object> subClasses = new List<object>();
+            foreach (var myType in allTypes)
+            {
+                bool isSubType = myType.IsSubclassOf(typeof(T));
+
+                if (isSubType)
+                {
+                    subClasses.Add(myType.Name);
+                }
+            }
+            return subClasses.ToArray();
+        }
+    }
+}
