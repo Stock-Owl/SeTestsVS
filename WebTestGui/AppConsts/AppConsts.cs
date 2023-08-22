46c2a8481cf6df04a66255eb6b625ae162cf9939
Kós Márton <kosmarci73@gmail.com>
2023-08-22 20:31:38 +0200

    To prevent merge confilcts

--- WebTestGui/AppConsts/AppConsts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebTestGui
-{
-    internal static class AppConsts
-    {
-        public static readonly string s_AppName = "VS WebTeszt";
-        public static readonly string s_AppVersion = "v1.0";
-        public static readonly string s_AppDefaultFileExtension = ".vstest";
-    }
-}
